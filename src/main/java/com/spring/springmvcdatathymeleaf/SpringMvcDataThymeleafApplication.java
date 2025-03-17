79cce7cd90aefd35555fda0226ab5e0df800e006
YassirJr <yassir.junior.03@gmail.com>
2025-03-17 15:05:41 +0000

    add the main class

---
+++ src/main/java/com/spring/springmvcdatathymeleaf/SpringMvcDataThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.spring.springmvcdatathymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDataThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDataThymeleafApplication.class, args);
+    }
+
+}
